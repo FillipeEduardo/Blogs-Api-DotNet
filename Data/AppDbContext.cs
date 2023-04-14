5cf95e3b190f55494566e3fedaaf710e57f88277
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2023-04-14 16:45:31 -0300

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blogs_Api_DotNet.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<> MyProperty { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Blogs_Api_DotNet.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
